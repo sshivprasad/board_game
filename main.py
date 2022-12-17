add23a13147e9881ca8d4f1368507490242dad2e
sshivprasad <shama.1502@hotmail.com>
2022-12-17 20:08:50 +0530

    Solution modeled into a class having a 2D-matrix of Cells

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from models import Board, Constraint, Cell
+from models import Board, Constraint, Cell, Solution
 from validator import is_solution_valid
 
 
 def main():
-    board = Board(n=4,
-                  constraints=[Constraint(Cell(1, 1), '>', Cell(1, 2)),
-                               Constraint(Cell(4, 1), '>', Cell(4, 2)),
-                               Constraint(Cell(3, 2), '<', Cell(4, 2)),
-                               Constraint(Cell(2, 3), '>', Cell(3, 3)),
-                               ]
+    n = 4
+    board = Board(n, constraints=[Constraint(Cell(1, 1), '>', Cell(1, 2)),
+                                  Constraint(Cell(4, 1), '>', Cell(4, 2)),
+                                  Constraint(Cell(3, 2), '<', Cell(4, 2)),
+                                  Constraint(Cell(2, 3), '>', Cell(3, 3)),
+                                  ]
                   )
 
-    solution = [[Cell(1, 1, 2), Cell(1, 2, 1), Cell(1, 3, 4), Cell(1, 4, 3)],
-                [Cell(2, 1, 1), Cell(2, 2, 4), Cell(2, 3, 3), Cell(2, 4, 2)],
-                [Cell(3, 1, 3), Cell(3, 2, 2), Cell(3, 3, 1), Cell(3, 4, 4)],
-                [Cell(4, 1, 4), Cell(4, 2, 3), Cell(4, 3, 2), Cell(4, 4, 1)],
-                ]
+    # cell values row-wise
+    cell_values = [2, 1, 4, 3, 1, 4, 3, 2, 3, 2, 1, 4, 4, 3, 2, 1]
+    valid_solution = Solution(n, cell_values)
+    is_valid, validator_msg = is_solution_valid(board, valid_solution)
+    print(f'{cell_values} - {is_valid} | {validator_msg}')
 
-    is_valid, validator_msg = is_solution_valid(board, solution)
-    print(f'{is_valid} | {validator_msg}')
+    # cell values row-wise
+    cell_values = [2, 1, 4, 3, 1, 4, 3, 2, 3, 1, 2, 4, 4, 3, 2, 1]
+    invalid_solution = Solution(n, cell_values)
+    is_valid, validator_msg = is_solution_valid(board, invalid_solution)
+    print(f'{cell_values} - {is_valid} | {validator_msg}')
 
 
 if __name__ == '__main__':
